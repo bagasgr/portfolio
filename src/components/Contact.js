7955376b569b5c006b6bdb079e172a14ab210244
bagasgr <bagasiyain205@gmail.com>
2025-01-27 23:28:05 +0700

    Tester

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -16,7 +16,7 @@ const Contact = () => {
     };
 
     try {
-      const response = await fetch("http://localhost:5000/send-email", {
+      const response = await fetch("http://localhost:5000/api/send-email", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
