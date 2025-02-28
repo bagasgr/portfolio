b4e3db13c7d116163bd7e3db6a1e86bc4a923f5d
Bagas Gilang Ramadhan <bagasiayin205@gmail.com>
2025-02-28 09:15:24 +0700

    MEMPERBARUI DSAIN

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Typewriter from "typewriter-effect";
 
 // Import Gambar
 import programmerImg from "./images/fota pro.jpeg";
@@ -33,13 +34,14 @@ const ScrollToTopButton = () => {
     };
   }, []);
 
-  return (
-    isVisible && (
-      <button onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })} className="scroll-to-top">
-       Back to Top
-      </button>
-    )
-  );
+  return isVisible ? (
+    <button
+      onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+      className="scroll-to-top"
+    >
+      Back to Top
+    </button>
+  ) : null;
 };
 
 function App() {
@@ -53,84 +55,150 @@ function App() {
     setNotification(null);
   };
 
+  
+
   return (
     <Router>
       <div className="App">
         {/* Hero Section */}
         <div className="hero-section">
           <div className="hero-content">
-            <h1 className="hero-title">I Am <span>Bagas Gilang Ramadhan</span></h1>
-            <p className="hero-description">A Software Developer passionate about building innovative and scalable solutions.</p>
+            <h1 className="hero-title">
+              <span className="iam">I Am</span>{" "}
+              <span className="name">Bagas Gilang Ramadhan</span>
+            </h1>
+            <h2 className="animated-title">
+              <Typewriter
+                options={{
+                  strings: [
+                    "Backend Developer",
+                    "Frontend Developer",
+                    "Fullstack Developer",
+                    "Cybersecurity Specialist",
+                    "Data Analyst",
+                    "Software Engineer",
+                    "Ethical Hacking",
+                    "Machine Learning",
+                    "AI Engineer",
+                  ],
+                  autoStart: true,
+                  loop: true,
+                  delay: 50,
+                  deleteSpeed: 20,
+                }}
+              />
+            </h2>
+            <p className="hero-description">
+            🔍 "I'm not just a developer, I'm a digital architect creating innovative solutions for the future."
+
+🔥 "Every line of code I write is not just an instruction, but a small step towards a technological revolution!"
+
+⚡ "In a world that is constantly changing, I choose to not only follow trends, but create them!"
+
+🌟 "My passion is not only understanding technology, but also how to use it to create a positive impact in the world."
+</p>
             <div className="navigation">
               <a href="/about">About</a>
               <a href="/pengalaman">Work Experience</a>
               <a href="/projects">Projects</a>
               <a href="/certifikat">Certifikat</a>
-              <a href="/pdf/BagasGilangRamadhan_softwareDeveloper(1).pdf" target="_blank" rel="noopener noreferrer">Resume</a>
+              <a
+                href="/pdf/BagasGilangRamadhan_softwareDeveloper(1).pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Resume
+              </a>
               <a href="/contact">Contact</a>
             </div>
           </div>
-          <img src={fotobagasImg} alt="Bagas Gilang Ramadhan" className="hero-image" />
+          <img
+            src={fotobagasImg}
+            alt="Bagas Gilang Ramadhan"
+            className="hero-image"
+          />
         </div>
 
         {/* Skill Section */}
         <div className="skills-section">
-  {[
-    { 
-      title: "Fullstack Developer", 
-      img: programmerImg, 
-      content: "With extensive experience as a Fullstack Developer, I have built and optimized various web applications, from e-commerce platforms to inventory management systems and online transportation services. I specialize in PHP (Native & CodeIgniter), JavaScript, and MySQL, ensuring seamless integration between frontend and backend. My expertise includes designing responsive UI/UX, implementing secure authentication, and optimizing database performance. I take pride in delivering scalable, efficient, and high-performing web applications that enhance user experience and business growth." 
-    },
-    { 
-      title: "Cybersecurity Specialist", 
-      img: cybersecurityImg, 
-      content: "I have a deep understanding of cybersecurity principles and best practices. I specialize in securing web applications against common threats such as SQL Injection, Cross-Site Scripting (XSS), and Cross-Site Request Forgery (CSRF). I implement encryption techniques, multi-factor authentication (MFA), and security monitoring to protect sensitive data. My knowledge extends to ethical hacking practices, vulnerability assessments, and penetration testing to identify and mitigate security risks before they become threats." 
-    },
-    { 
-      title: "Data Analyst", 
-      img: dataImg, 
-      content: "I am skilled in data analysis and business intelligence, using SQL to extract valuable insights and optimize decision-making. I have worked with large datasets to identify patterns and trends, leveraging machine learning algorithms such as K-Nearest Neighbors (KNN) for predictive analytics. With expertise in Python, Pandas, and Matplotlib, I create compelling data visualizations that translate raw numbers into actionable business strategies. My analytical mindset enables me to drive efficiency and innovation through data-driven solutions." 
-    },
-    { 
-      title: "Software Engineer", 
-      img: softwareImg, 
-      content: "As a Software Engineer, I excel in designing, developing, and maintaining robust software applications. I follow industry best practices, including clean code principles, modular programming, and Agile methodologies. My approach involves thorough system analysis using Unified Modeling Language (UML) diagrams, ensuring a structured development process. I have successfully built high-performance applications that enhance productivity, optimize business processes, and meet evolving technological demands." 
-    },
-    { 
-      title: "DevOps Engineer", 
-      img: devImg, 
-      content: "I have hands-on experience in DevOps, managing server configurations, CI/CD pipelines, and cloud deployments. I have worked with AWS, DigitalOcean, and on-premise hosting solutions, ensuring seamless deployment and scalability. My expertise includes setting up automated build and deployment pipelines, optimizing system performance, and maintaining server security. By integrating DevOps practices, I enhance development efficiency, minimize downtime, and streamline collaboration between development and operations teams." 
-    },
-    { 
-      title: "Ethical Hacking", 
-      img: etImg, 
-      content: "With a passion for cybersecurity, I have developed a solid foundation in ethical hacking. I understand attack vectors, penetration testing methodologies, and network security protocols. I have conducted security assessments to identify vulnerabilities in web applications and network infrastructures. By utilizing ethical hacking tools and techniques, I help strengthen system defenses, ensuring data protection and compliance with security standards." 
-    },
-    { 
-      title: "Machine Learning", 
-      img: macinImg, 
-      content: "I have experience in machine learning and artificial intelligence, particularly in classification and predictive analytics. I have applied K-Nearest Neighbors (KNN) and Decision Tree algorithms to solve real-world problems, such as student graduation predictions. Using Python and libraries like Scikit-Learn, I build models that analyze data patterns and provide accurate forecasts. My knowledge of AI enables me to develop intelligent applications that drive automation and enhance decision-making processes." 
-    },
-    { 
-      title: "Project Manager IT", 
-      img: proImg, 
-      content: "As an IT Project Manager, I excel in leading teams, managing software development lifecycles, and ensuring project success. I have experience in Agile and Scrum methodologies, coordinating between developers, designers, and stakeholders to achieve project goals efficiently. I have successfully delivered multiple software projects on time and within budget by implementing strategic planning, risk management, and continuous progress tracking. My leadership skills help foster collaboration and drive innovation within development teams." 
-    }
-  ].map((skill, index) => (
-    <div className="skill-card" key={index} onClick={() => handleImageClick(skill.title, skill.content)}>
-      <img src={skill.img} alt={skill.title} className="skill-image" />
-      <h3 className="skill-title">{skill.title}</h3>
-    </div>
-  ))}
-</div>
-
+          {[
+            {
+              title: "Fullstack Developer",
+              img: programmerImg,
+              content:
+                "Fullstack Developer With expertise in both frontend and backend development, I craft dynamic and responsive web applications. Using technologies like JavaScript, PHP, and SQL, I build seamless user experiences and scalable architectures. My proficiency in HTML, CSS, and frameworks like React ensures aesthetically pleasing designs, while my backend skills in database management and API development enable efficient data handling and secure operations.",
+            },
+            {
+              title: "Cybersecurity Specialist",
+              img: cybersecurityImg,
+              content:
+                "Cybersecurity Specialist I specialize in securing digital infrastructures by implementing robust security measures and risk assessments. With knowledge of network security, penetration testing, and cryptography, I safeguard systems against cyber threats. My experience includes vulnerability analysis, firewalls, and SIEM tools to ensure compliance and data protection. I am adept at ethical hacking to preemptively identify and mitigate security risks.",
+            },
+            {
+              title: "Data Analyst",
+              img: dataImg,
+              content:
+                "Data Analyst I transform raw data into actionable insights through statistical analysis and data visualization. Using tools like Python, SQL, and Power BI, I analyze trends and patterns that drive strategic decision-making. My expertise in data preprocessing, predictive modeling, and business intelligence helps organizations optimize performance and achieve measurable growth.",
+            },
+            {
+              title: "Software Engineer",
+              img: softwareImg,
+              content:
+                "Software Engineer I design, develop, and maintain software solutions with a strong foundation in object-oriented programming. Proficient in Java, C++, and Python, I build scalable applications that meet diverse business needs. My experience in software lifecycle management, debugging, and performance optimization ensures reliable and efficient software products that enhance user engagement and productivity.",
+            },
+            {
+              title: "DevOps Engineer",
+              img: devImg,
+              content:
+                "DevOps Engineer I bridge the gap between development and operations, streamlining workflows through CI/CD pipelines and automation. With expertise in Docker, Kubernetes, and cloud platforms like AWS, I enhance deployment efficiency and scalability. My knowledge of infrastructure as code (IaC) and system monitoring ensures optimal system performance and minimal downtime.",
+            },
+            {
+              title: "Ethical Hacking",
+              img: etImg,
+              content:
+                "Ethical Hacking I proactively identify vulnerabilities in systems through penetration testing and security assessments. My knowledge of ethical hacking methodologies, Kali Linux, and exploit frameworks like Metasploit allows me to test and strengthen defenses. By simulating cyberattacks, I provide organizations with critical insights to enhance security postures and prevent real-world breaches.",
+            },
+            {
+              title: "Machine Learning",
+              img: macinImg,
+              content:
+                "Machine Learning I develop intelligent models that leverage data-driven decision-making using Python, TensorFlow, and scikit-learn. My expertise in supervised and unsupervised learning enables me to build predictive algorithms for applications like image recognition, natural language processing, and anomaly detection. I optimize model performance to drive innovation and automation.",
+            },
+            {
+              title: "Project Manager IT",
+              img: proImg,
+              content:
+                "Project Manager IT I lead IT projects from inception to completion, ensuring seamless execution within scope, time, and budget. My skills in Agile, Scrum, and stakeholder management enable efficient coordination between teams. I focus on risk assessment, resource allocation, and performance tracking to drive project success while maintaining high-quality deliverables and stakeholder satisfaction.",
+            },
+          ].map((skill, index) => (
+            <div
+              className="skill-card"
+              key={index}
+              onClick={() =>
+                handleImageClick(skill.title, skill.content)
+              }
+            >
+              <img
+                src={skill.img}
+                alt={skill.title}
+                className="skill-image"
+              />
+              <h3 className="skill-title">{skill.title}</h3>
+            </div>
+          ))}
+        </div>
 
         {/* Notification Section */}
         {notification && (
           <div className="notification">
             <h2>{notification.title}</h2>
             <p>{notification.content}</p>
-            <button className="close-button" onClick={closeNotification}>Close</button>
+            <button
+              className="close-button"
+              onClick={closeNotification}
+            >
+              Close
+            </button>
           </div>
         )}
 
